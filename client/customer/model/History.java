62d53620194a41862693b13c027cecb2ffdff270
KENJHIE CARL MOGATAS <2240909@slu.edu.ph>
2025-03-22 17:45:51 +0000

    Replace History.java

--- client/customer/model/History.java
+++ client/customer/model/History.java
@@ -1,80 +1,36 @@
 package client.customer.model;
 
-import server.customer.LogServer;
-import org.w3c.dom.*;
-import javax.xml.parsers.*;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.*;
+import server.ServerInterface;
+
+import java.rmi.RemoteException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class History {
-    private File userFile;
+    private final String username;
+    private final ServerInterface server;
 
-    public History(String userFile) {
-        this.userFile = new File(userFile);
-    } // ALL WILL BE CONVERTED AS RMI AND JSON
+    public History(String username, ServerInterface server) {
+        this.username = username;
+        this.server = server;
+    }
 
     public List<String[]> loadHistory() {
-        List<String[]> historyRecords = new ArrayList<>();
         try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(userFile);
-            doc.getDocumentElement().normalize();
-
-            NodeList bookingNodes = doc.getElementsByTagName("booking");
-            for (int i = 0; i < bookingNodes.getLength(); i++) {
-                Element bookingElement = (Element) bookingNodes.item(i);
-                String machineType = bookingElement.getElementsByTagName("machineType").item(0).getTextContent();
-                String date = bookingElement.getElementsByTagName("date").item(0).getTextContent();
-                String time = bookingElement.getElementsByTagName("time").item(0).getTextContent();
-                NodeList commentNodes = bookingElement.getElementsByTagName("comment");
-                String comment = commentNodes.getLength() > 0 ? commentNodes.item(0).getTextContent() : "";
-                historyRecords.add(new String[]{machineType, date, time, comment});
-            }
-        } catch (Exception e) {
+            return server.loadHistory(username);
+        } catch (RemoteException e) {
             e.printStackTrace();
+            return new ArrayList<>();
         }
-        return historyRecords;
     }
 
     public void updateComment(String machineType, String date, String time, String comment) {
         try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(userFile);
-            doc.getDocumentElement().normalize();
-
-            NodeList bookingNodes = doc.getElementsByTagName("booking");
-            for (int i = 0; i < bookingNodes.getLength(); i++) {
-                Element bookingElement = (Element) bookingNodes.item(i);
-                String existingMachine = bookingElement.getElementsByTagName("machineType").item(0).getTextContent();
-                String existingDate = bookingElement.getElementsByTagName("date").item(0).getTextContent();
-                String existingTime = bookingElement.getElementsByTagName("time").item(0).getTextContent();
-
-                if (existingMachine.equals(machineType) && existingDate.equals(date) && existingTime.equals(time)) {
-                    NodeList commentNodes = bookingElement.getElementsByTagName("comment");
-                    if (commentNodes.getLength() > 0) {
-                        commentNodes.item(0).setTextContent(comment);
-                    } else {
-                        Element commentElement = doc.createElement("comment");
-                        commentElement.setTextContent(comment);
-                        bookingElement.appendChild(commentElement);
-                    }
-                    break;
-                }
-            }
-
-            Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.transform(new DOMSource(doc), new StreamResult(userFile));
-
-            String username = userFile.getName().replace(".xml", "");
-            LogServer.log(username + " commented on " + machineType + " at " + date + ", " + time);
-        } catch (Exception e) {
+            server.updateComment(username, machineType, date, time, comment);
+        } catch (RemoteException e) {
             e.printStackTrace();
         }
     }
@@ -104,72 +60,8 @@ public class History {
 
     public void deleteBooking(String machineType, String date, String time) {
         try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(userFile);
-            doc.getDocumentElement().normalize();
-
-            NodeList bookingNodes = doc.getElementsByTagName("booking");
-            Node bookingToDelete = null;
-            for (int i = 0; i < bookingNodes.getLength(); i++) {
-                Element bookingElement = (Element) bookingNodes.item(i);
-                String existingMachine = bookingElement.getElementsByTagName("machineType").item(0).getTextContent();
-                String existingDate = bookingElement.getElementsByTagName("date").item(0).getTextContent();
-                String existingTime = bookingElement.getElementsByTagName("time").item(0).getTextContent();
-
-                if (existingMachine.equals(machineType) && existingDate.equals(date) && existingTime.equals(time)) {
-                    bookingToDelete = bookingElement;
-                    break;
-                }
-            }
-
-            if (bookingToDelete != null) {
-                bookingToDelete.getParentNode().removeChild(bookingToDelete);
-                Transformer transformer = TransformerFactory.newInstance().newTransformer();
-                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-                transformer.transform(new DOMSource(doc), new StreamResult(userFile));
-                markSlotAsVacant(machineType, date, time);
-
-                String username = userFile.getName().replace(".xml", "");
-                LogServer.log(username + " cancelled " + machineType + " at " + date + ", " + time);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void markSlotAsVacant(String machineType, String date, String time) {
-        try {
-            File scheduleFile = new File("schedule.xml");
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(scheduleFile);
-            doc.getDocumentElement().normalize();
-
-            NodeList machineNodes = doc.getElementsByTagName(machineType.toLowerCase());
-            for (int i = 0; i < machineNodes.getLength(); i++) {
-                Element machineElement = (Element) machineNodes.item(i);
-                NodeList dayNodes = machineElement.getElementsByTagName("day");
-                for (int j = 0; j < dayNodes.getLength(); j++) {
-                    Element dayElement = (Element) dayNodes.item(j);
-                    if (dayElement.getAttribute("date").equals(date)) {
-                        NodeList timeSlots = dayElement.getElementsByTagName("time");
-                        for (int k = 0; k < timeSlots.getLength(); k++) {
-                            Element timeElement = (Element) timeSlots.item(k);
-                            if (timeElement.getAttribute("slot").equals(time)) {
-                                timeElement.setTextContent("VACANT");
-                                break;
-                            }
-                        }
-                        break;
-                    }
-                }
-            }
-
-            Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.transform(new DOMSource(doc), new StreamResult(scheduleFile));
-        } catch (Exception e) {
+            server.deleteBooking(username, machineType, date, time);
+        } catch (RemoteException e) {
             e.printStackTrace();
         }
     }
