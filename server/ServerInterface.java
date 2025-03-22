3e0011324121fb7e67f90ff8087b8c619e22563a
KENJHIE CARL MOGATAS <2240909@slu.edu.ph>
2025-03-22 17:43:55 +0000

    Replace ServerInterface.java

--- server/ServerInterface.java
+++ server/ServerInterface.java
@@ -3,12 +3,19 @@ package server;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import com.google.gson.JsonObject;
+import java.util.List;
+
 
 /**
  * RMI interface for handling server operations with Gson-based JSON communication.
  */
 public interface ServerInterface extends Remote {
-
+    List<String[]> loadHistory(String username) throws RemoteException;
+    void updateComment(String username, String machineType, String date, String time, String comment) throws RemoteException;
+    void deleteBooking(String username, String machineType, String date, String time) throws RemoteException;
+    List<String> getTimeSlotsForDate(String machineType, String date) throws RemoteException;
+    String getSlotStatus(String machineType, String date, String timeSlot) throws RemoteException;
+    boolean updateSlotStatus(String machineType, String date, String timeSlot, String status) throws RemoteException;
     String getTimeSlots() throws RemoteException;
 
     boolean validateUserCredentials(String username, String password) throws RemoteException;
